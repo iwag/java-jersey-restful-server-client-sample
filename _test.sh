26c0506d7e3df46ea848a0043fd363d78fd1d08f
iwag <iwagame@gmail.com>
2017-08-14 03:51:34 -0700

    simple CRUD server

--- _test.sh
+++ _test.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-cd $TRAVIS_BUILD_DIR/projclient
-mvn clean test
-cd $TRAVIS_BUILD_DIR/projserver
-mvn clean test
+cd $TRAVIS_BUILD_DIR/projclient && mvn clean test && cd ../projserver && mvn clean test
--- projserver/src/main/java/io/github/iwag/jerseystarter/resources/TaskResource.java
+++ projserver/src/main/java/io/github/iwag/jerseystarter/resources/TaskResource.java
@@ -1,17 +1,40 @@
 package io.github.iwag.jerseystarter.resources;
 
-import io.github.iwag.jerseystarter.Task;
+import io.github.iwag.jerseystarter.models.Task;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("task")
 public class TaskResource {
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public Task create() {
+    public Task get() {
         return new Task("sample", 0, "2017/08/10");
+    }
+
+    @DELETE
+    public Response delete() {
+        // write deleting code
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response update(Task t) {
+        System.out.println(t);
+        // write deleting code
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+
+    @PUT
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response create(Task t) {
+        System.out.println(t);
+        // write creating code
+        return Response.status(Response.Status.ACCEPTED).build();
     }
 
 }
