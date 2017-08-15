58e09e3279e3f05c7a8b679541f443ff7aa8df4c
iwag <iwagame@gmail.com>
2017-08-14 18:17:16 -0700

    Add simple test in server

--- projserver/src/test/java/io/github/iwag/jerseystarter/TaskTest.java
+++ projserver/src/test/java/io/github/iwag/jerseystarter/TaskTest.java
@@ -1,8 +1,11 @@
 package io.github.iwag.jerseystarter;
 
+import io.github.iwag.jerseystarter.models.Task;
 import io.github.iwag.jerseystarter.resources.TaskResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
+import org.junit.Test;
 
 import javax.ws.rs.core.Application;
 
@@ -10,5 +13,13 @@ public class TaskTest extends JerseyTest {
     @Override
     protected Application configure() {
         return new ResourceConfig(TaskResource.class);
+    }
+
+    @Test
+    public void testGetTask0Normal() throws Exception {
+        Task task = target("task/0").request().get(Task.class);
+        Task expected = new Task("sample", 0, "2017/08/10");
+
+        Assert.assertEquals(task, expected);
     }
 }
