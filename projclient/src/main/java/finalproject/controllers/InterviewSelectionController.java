04d7e8d1141568f5c5188ca2de7ec46d07d771e1
iwag <iwagame@gmail.com>
2017-08-15 19:10:43 -0700

    change desgin of controllers

--- projclient/src/main/java/finalproject/controllers/InterviewSelectionController.java
+++ projclient/src/main/java/finalproject/controllers/InterviewSelectionController.java
@@ -1,11 +1,10 @@
 package finalproject.controllers;
 
-import finalproject.models.entities.HistoryEntity;
-import finalproject.models.entities.InterviewEntity;
-import finalproject.models.entities.InterviewSelectionModel;
-import finalproject.models.entities.ProfileEntity;
+import finalproject.models.entities.*;
 import finalproject.models.managers.APIManager;
+import finalproject.models.managers.CredentialManager;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class InterviewSelectionController {
@@ -15,13 +14,50 @@ public class InterviewSelectionController {
         this.apiManager = apiManager;
     }
 
-    public InterviewEntity interview() {
+    public InterviewEntity interview(CredentialManager credentialManager) {
 
         Scanner scanner = new Scanner(System.in);
-//        String topic = scanner.next();
-        String topic = "Java";
+        InterviewEntity ie = null;
+        do {
+            System.out.println("Welcome to the Mock-up Interview:");
+            System.out.println("* Start an interview in Java [Press J]");
+            System.out.println("* Start an interview in SQL[Press S]");
+            System.out.println("* See the history of past interviews [Press H] * Return to Main Menu [Press M]            ");
+            String key = scanner.next();
+            if (key.startsWith("M")) {
+                System.out.println("return");
+                break;
+            } else if (key.startsWith("J")) {
+                ie = interview("Java", apiManager, credentialManager);
+            } else if (key.startsWith("S")) {
+                ie = interview("SQL", apiManager, credentialManager);
+            } else if (key.startsWith("H")) {
+                history(apiManager, credentialManager);
+            }
 
-        InterviewSelectionModel selectionModel = new InterviewSelectionModel(topic);
-        return apiManager.interview(selectionModel);
+        } while (ie == null);
+        return ie;
     }
+    private static void history(APIManager apiManager, CredentialManager credentialManager) {
+        if (credentialManager.getPe() == null) {
+            System.out.println("You have to login. Thank you");
+            return ;
+        }
+        HistoryController historyController = new HistoryController(apiManager);
+        HistoryEntity he = historyController.history(credentialManager.getPe());
+        System.err.print(he);
+    }
+
+    private static InterviewEntity interview(String topic, APIManager apiManager, CredentialManager credentialManager) {
+        if (credentialManager.getPe() == null) {
+            System.out.println("You have to login. Thank you");
+            return null;
+        }
+
+        InterviewSelectionModel selection = new InterviewSelectionModel(topic);
+        InterviewEntity interviewEntity =  apiManager.getInterviewer(selection);
+
+        return interviewEntity;
+    }
+
 }
