0a94f8e28d21564412ecc23eaf2e8bf3feebd109
iwag <iwagame@gmail.com>
2017-08-21 02:26:23 -0700

    fix server's injection

--- projserver/src/main/java/io/github/iwag/finalproj/main/OurConfig.java
+++ projserver/src/main/java/io/github/iwag/finalproj/main/OurConfig.java
@@ -1,24 +1,30 @@
 package io.github.iwag.finalproj.main;
 
 
+import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 
 import io.github.iwag.finalproj.store.MySQLUserStore;
 import io.github.iwag.finalproj.store.UserStore;
+import org.glassfish.hk2.api.DynamicConfiguration;
+import org.glassfish.hk2.api.DynamicConfigurationService;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.BuilderHelper;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("rest")
+@ApplicationPath("/")
 public class OurConfig extends ResourceConfig {
 
-    public OurConfig() {
-        packages(this.getClass().getPackage().getName());
+    @Inject
+    public OurConfig(ServiceLocator locator) {
+        DynamicConfigurationService dcs = locator.getService(DynamicConfigurationService.class);
+        DynamicConfiguration config = dcs.createDynamicConfiguration();
 
-        register(new AbstractBinder() {
-            @Override
-            protected void configure() {
-                bind(MySQLUserStore.class).to(UserStore.class);
-            }
-        });
+        config.bind(BuilderHelper.link(MySQLUserStore.class).to(UserStore.class).build());
+
+        config.commit();
+
+        packages(true, getClass().getPackage().getName());
     }
 }
